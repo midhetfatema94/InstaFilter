93d133c62f8b30901b32e688807a5c92455c0464
Midhet Sulemani <midhetfatema94@gmail.com>
2020-12-25 17:07:28 +0300

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Midhet Sulemani on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
