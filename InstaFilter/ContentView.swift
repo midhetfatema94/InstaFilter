090aa94dbb7b4ceb24e85baacf77b5deba71c99e
Midhet Sulemani <midhetfatema94@gmail.com>
2020-12-27 19:57:06 +0300

    - integrating core image with swiftui

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -6,11 +6,52 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button(action: {
+                self.showingImagePicker = true
+            }, label: {
+                Text("Select Image")
+            })
+        }
+        .sheet(isPresented: $showingImagePicker, content: {
+            ImagePicker()
+        })
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.pixellate()
+        
+        currentFilter.inputImage = beginImage
+        currentFilter.scale = 100
+        
+        // get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        // attempt to get a CGImage from our CIImage
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // convert that to a UIImage
+            let uiImage = UIImage(cgImage: cgimg)
+
+            // and convert that to a SwiftUI image
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
---
+++ InstaFilter/ImagePicker.swift
@@ -0,0 +1,19 @@
+//
+//  ImagePicker.swift
+//  InstaFilter
+//
+//  Created by Waveline Media on 12/27/20.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+}
